bdc83e8685d834aa293a9160cb493035fcf22547
jcosta86 <jeferson.costa@olist.com>
2021-02-08 19:47:04 -0300

    criando models de sport e match

--- boteco/bet/admin.py
+++ boteco/bet/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
 
-from .models import Team
+from .models import Team, Sport, Match
 
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
 
 
+class SportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = (
+        'match_location',
+        'match_date',
+        'id_team_1',
+        'id_team_2',
+        'id_sport',
+        'score_team_1',
+        'score_team_2')
+
+
 admin.site.register(Team, TeamAdmin)
+admin.site.register(Sport, SportAdmin)
+admin.site.register(Match, MatchAdmin)
--- boteco/bet/models.py
+++ boteco/bet/models.py
@@ -6,4 +6,25 @@ class Team(models.Model):
     description = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"Team: {self.name} - Description: {self.description}"
+        return self.name
+
+
+class Sport(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    match_location = models.CharField(max_length=100)
+    id_team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, null=False, related_name='+')
+    id_team_2 = models.ForeignKey(Team, on_delete=models.CASCADE, null=False, related_name='+')
+    id_sport = models.ForeignKey(Sport, on_delete=models.CASCADE, null=False)
+    match_date = models.DateTimeField(null=False)
+    score_team_1 = models.IntegerField(default=0)
+    score_team_2 = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Match location: {self.match_location} Date: {self.match_date}"
